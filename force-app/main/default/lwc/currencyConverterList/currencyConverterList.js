dadec472c0f6db971e4d097f836ad99052b6315c
Movsar Bekaev <bekaev.movsar@gmail.com>
2021-03-28 13:33:00 +0300

    savepoint

--- force-app/main/default/lwc/currencyConverterList/currencyConverterList.js
+++ force-app/main/default/lwc/currencyConverterList/currencyConverterList.js
@@ -1,7 +1,7 @@
 import { LightningElement, api } from 'lwc';
 
 export default class CurrencyConverterList extends LightningElement {
-    @api base;
+    @api baseCurrency;
     @api rates;
     @api currentPageRates;
     @api currentPage;
--- force-app/main/default/lwc/currencyConverterListItem/currencyConverterListItem.js
+++ force-app/main/default/lwc/currencyConverterListItem/currencyConverterListItem.js
@@ -2,13 +2,13 @@ import { LightningElement, api } from 'lwc';
 
 export default class CurrencyConverterListItem extends LightningElement {
     @api rate;
-    @api base;
+    @api baseCurrency;
     @api marked = false;
 
     basicStyle = 'slds-button slds-col slds-size_1-of-1';
 
     get rateStyle(){
-        if (this.rate.code === this.base){
+        if (this.rate.code === this.baseCurrency){
             return `${this.basicStyle} slds-button_brand`;
         }
         return `${this.basicStyle} slds-button_outline-brand`;

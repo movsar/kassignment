1594c38a14f97ac80a1bba9568b8f72bb6fd58ec
Movsar Bekaev <bekaev.movsar@gmail.com>
2021-03-29 06:29:16 +0300

    reCalculate test works

--- force-app/main/default/lwc/currencyConverterCalc/__tests__/currencyConverterCalc.test.js
+++ force-app/main/default/lwc/currencyConverterCalc/__tests__/currencyConverterCalc.test.js
@@ -1,7 +1,7 @@
 import { createElement } from 'lwc';
 import CurrencyConverterCalc from 'c/currencyConverterCalc';
 const mockRates = require('./mockRates.json');
-
+const flushPromises = () => new Promise(setImmediate);
 describe('c-currency-converter-calc', () => {
     afterEach(() => {
         // The jsdom instance is shared across test cases in a single file so reset the DOM
@@ -10,19 +10,26 @@ describe('c-currency-converter-calc', () => {
         }
     });
 
-    it('ensures it works', () => {
+    it('ensures it works', async () => {
         const element = createElement('c-currency-converter-calc', {
             is: CurrencyConverterCalc
         });
         element.rates = mockRates;
         element.baseCurrency = 'USD';
         element.quoteCurrency = 'RUB';
-        document.body.appendChild(element);
 
-        // Use a promise to wait for asynchronous changes to the DOM
-        return Promise.resolve().then(() => {
-            const amountInBaseCurrencyElement = element.shadowRoot.querySelector('[data-id=amountInBaseCurrency]');
-            expect(parseInt(amountInBaseCurrencyElement.value)).toBe(1);
-        });
+        document.body.appendChild(element);
+        // Wait for the renderer
+        await flushPromises();
+
+        const amountInBaseCurrencyElement = element.shadowRoot.querySelector('[data-id=amountInBaseCurrency]');
+        const amountInQouteCurrencyElement = element.shadowRoot.querySelector('[data-id=amountInQuoteCurrency]');
+       
+        element.reCalculate(mockRates, 'BASE_TO_QUOTE');
+        // Wait for the renderer
+        await flushPromises();
+        
+        expect(parseInt(amountInBaseCurrencyElement.value)).toBe(1);
+        expect(parseFloat(amountInQouteCurrencyElement.value)).toBe(104.683);
     });
 });
